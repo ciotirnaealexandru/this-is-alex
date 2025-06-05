08929dccf893c5bbb3e6ee18700933156b381d5b
Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>
2025-06-06 00:10:20 +0300

    Setup with vite, tailwind, react router etc
    Signed-off-by: Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route, Navigate } from "react-router";
+import { useEffect, useState } from "react";
+import About from "./pages/About/About"
+
+function App() {
+  const [firstLoad, setFirstLoad] = useState(true);
+
+  useEffect(() => {
+    if (firstLoad) {
+      setFirstLoad(false);
+    }
+  }, []);
+
+  return (
+    <Routes>
+      {firstLoad && (
+        <Route path="/" element={<Navigate to="/about" replace />} />
+      )}
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/About/About.tsx
@@ -0,0 +1,6 @@
+const About = () => {
+    return <div>lol</div>;
+};
+
+export default About;
+
