364d05ec32be359178129054d3ca54007a883185
Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>
2025-06-06 11:28:59 +0300

    Add tailwind typography for markdown files
    Signed-off-by: Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -1,9 +1,15 @@
 import NavBar from "../../components/Navbar/NavBar";
+import Markdown from "react-markdown";
+
+const markdown = "# Hi, *Alex*!";
 
 const About = () => {
   return (
-    <div className="bg-gray-950 w-screen h-screen pt-0.5">
+    <div className="bg-blue-500 w-screen h-screen pt-0.5">
       <NavBar />
+      <div className="prose">
+        <Markdown>{markdown}</Markdown>
+      </div>
     </div>
   );
 };
